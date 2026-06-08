984459bf83c9d23997bf06eaaa2a5021c71c26b5
Jaishree Vanuvamalai <jeyshreev@gmail.com>
2026-06-08 18:46:11 +0530

    Update 01_raw_tables.sql

--- sql/01_raw_tables.sql
+++ sql/01_raw_tables.sql
@@ -19,23 +19,3 @@ CREATE TABLE raw.current_pm_confirmation (
 	unit varchar(255) NULL,
 	commercial_model varchar(100) NULL
 );
-CREATE TABLE raw.fact_pm_history (
-	project_id varchar(50) NULL,
-	project_name varchar(255) NULL,
-	project_customer varchar(255) NULL,
-	project_vertical varchar(255) NULL,
-	project_execution_unit varchar(255) NULL,
-	project_type varchar(100) NULL,
-	project_status varchar(100) NULL,
-	project_manager_id varchar(50) NULL,
-	project_manager_name varchar(255) NULL,
-	asdm_number varchar(50) NULL,
-	asdm_name varchar(255) NULL,
-	pm_status varchar(50) NULL,
-	billing_cycle_start_date date NULL,
-	billing_cycle_end_date date NULL,
-	vertical varchar(255) NULL,
-	unit varchar(255) NULL,
-	commercial_model varchar(100) NULL,
-	snapshot_datetime text NULL
-);
